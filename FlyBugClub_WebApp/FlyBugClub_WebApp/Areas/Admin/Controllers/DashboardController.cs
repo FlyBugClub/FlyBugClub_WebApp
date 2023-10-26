f63bbd05de1c52a24a12593cb127ced9e6809725
MinhDuc <taduc1130@gmail.com>
2023-10-26 22:51:45 +0700

    update Duc

--- FlyBugClub_WebApp/FlyBugClub_WebApp/Areas/Admin/Controllers/DashboardController.cs
+++ FlyBugClub_WebApp/FlyBugClub_WebApp/Areas/Admin/Controllers/DashboardController.cs
@@ -1,11 +1,15 @@
-﻿using FlyBugClub_WebApp.Areas.Identity.Data;
+using FlyBugClub_WebApp.Areas.Identity.Data;
 using FlyBugClub_WebApp.Models;
 using FlyBugClub_WebApp.Repository;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+
 using NuGet.Protocol.Core.Types;
+
+using Org.BouncyCastle.Asn1.X509;
+
 using System.Data;
 
 namespace FlyBugClub_WebApp.Areas.Admin.Controllers
@@ -26,21 +30,45 @@ namespace FlyBugClub_WebApp.Areas.Admin.Controllers
             _dashboard = dashboard;
         }
 
-        public IActionResult Dashboard()  //Trang Dashboard chính của admin
+        public IActionResult Dashboard(int day, int month, int year)  //Trang Dashboard chính của admin
         {
-            int currentYear = DateTime.Now.Year;
+              int currentYear = DateTime.Now.Year;
 
-            // Tính tổng số lượng thuê thiết bị qua tất cả các đơn hàng
-            int totalQuantity = _ctx.BillBorrows.SelectMany(x => x.BorrowDetails).Sum(d => d.Quantity);
-            ViewBag.TotalQuantity = totalQuantity;
+              // Tính tổng số lượng thuê thiết bị qua tất cả các đơn hàng
+              int totalQuantity = _ctx.BillBorrows.SelectMany(x => x.BorrowDetails).Sum(d => d.Quantity);
+              ViewBag.TotalQuantity = totalQuantity;
 
-            // Tính tổng tiền từ các billBorrow có Status là true
-            var totalAmount = _ctx.BillBorrows.Where(x => x.Status == 1 && x.BorrowDate.Year == currentYear).Sum(b => b.Total);
-            ViewBag.TotalAmount = totalAmount;
+              // Tính tổng tiền từ các billBorrow có Status là true
+              var totalAmount = _ctx.BillBorrows.Where(x => x.Status == 1 && x.BorrowDate.Year == currentYear).Sum(b => b.Total);
+              ViewBag.TotalAmount = totalAmount;
 
-            // Đếm số lượng BillBorrow có Status là true
-            int countTrue = _ctx.BillBorrows.Count(b => b.Status == 1);
-            ViewBag.countTrue = countTrue;
+              // Đếm số lượng BillBorrow có Status là true
+              int countTrue = _ctx.BillBorrows.Count(b => b.Status == 1);
+              ViewBag.countTrue = countTrue;
+
+
+            // xử lý bill borrow
+            List<BillBorrow> billBorrows = new List<BillBorrow>();
+
+            if (day > 0 && month > 0 && year > 0)
+            {
+                // Tạo một đối tượng kiểu DateTime từ dữ liệu ngày, tháng, năm nhận được
+                var targetDate = new DateTime(year, month, day);
+
+                // Truy vấn dữ liệu theo ngày
+                billBorrows = _ctx.BillBorrows
+                    .Where(b => b.BorrowDate.Date == targetDate.Date)
+                    .ToList();
+            }
+            else
+            {
+                // Xử lý trường hợp không có ngày, tháng, năm được cung cấp
+                billBorrows = _ctx.BillBorrows.ToList();
+            }
+
+
+
+
 
             return View();
         }
@@ -50,5 +78,7 @@ namespace FlyBugClub_WebApp.Areas.Admin.Controllers
             _signInManager.SignOutAsync();
             return LocalRedirect("/identity/account/login");
         }
+
+       
     }
 }
