6afafd648b9fe8b629df361b4e916f3ed6daf516
MinhDuc <taduc1130@gmail.com>
2023-11-02 12:24:59 +0700

    Hello anh Duy :))

--- FlyBugClub_WebApp/FlyBugClub_WebApp/Areas/Admin/Controllers/MemberController.cs
+++ FlyBugClub_WebApp/FlyBugClub_WebApp/Areas/Admin/Controllers/MemberController.cs
@@ -4,6 +4,8 @@ using FlyBugClub_WebApp.Repository;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using System.Data;
 
 namespace FlyBugClub_WebApp.Areas.Admin.Controllers
@@ -12,21 +14,158 @@ namespace FlyBugClub_WebApp.Areas.Admin.Controllers
     [Authorize(Roles = "Administrator")]
     public class MemberController : Controller
     {
-        private readonly FlyBugClubWebApplicationContext _ctx;
-       
-        
-        public MemberController(FlyBugClubWebApplicationContext ctx)
+        private readonly FlyBugClubWebApplicationContext _context;
+
+        public MemberController(FlyBugClubWebApplicationContext context)
         {
-            _ctx = ctx;
+            _context = context;
         }
 
         public IActionResult Member()
         {
             // Sử dụng LINQ để lấy tất cả người dùng từ DbContext
-            List<User> lst = _ctx.Users.ToList();
+            List<User> lst = _context.Users.ToList();
 
             return View("Member", lst);
         }
-        
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null || _context.Users == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users
+                .Include(u => u.Position)
+                .FirstOrDefaultAsync(m => m.StudentId == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // GET: Admin/Users/Create
+        public IActionResult Create()
+        {
+            ViewData["PositionId"] = new SelectList(_context.Positions, "PositionId", "PositionId");
+            return View();
+        }
+
+        // POST: Admin/Users/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("StudentId,Name,Gender,Phone,Address,Email,Major,Faculty,ImgUser,PositionId,Account")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(user);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["PositionId"] = new SelectList(_context.Positions, "PositionId", "PositionId", user.PositionId);
+            return View(user);
+        }
+
+        // GET: Admin/Users/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null || _context.Users == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            ViewData["PositionId"] = new SelectList(_context.Positions, "PositionId", "PositionId", user.PositionId);
+            return View(user);
+        }
+
+        // POST: Admin/Users/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("StudentId,Name,Gender,Phone,Address,Email,Major,Faculty,ImgUser,PositionId,Account")] User user)
+        {
+            if (id != user.StudentId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(user);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserExists(user.StudentId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["PositionId"] = new SelectList(_context.Positions, "PositionId", "PositionId", user.PositionId);
+            return View(user);
+        }
+
+        // GET: Admin/Users/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null || _context.Users == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users
+                .Include(u => u.Position)
+                .FirstOrDefaultAsync(m => m.StudentId == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // POST: Admin/Users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            if (_context.Users == null)
+            {
+                return Problem("Entity set 'FlyBugClubWebApplicationContext.Users'  is null.");
+            }
+            var user = await _context.Users.FindAsync(id);
+            if (user != null)
+            {
+                _context.Users.Remove(user);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UserExists(string id)
+        {
+            return (_context.Users?.Any(e => e.StudentId == id)).GetValueOrDefault();
+        }
+
+
     }
 }
