7deb8b7aff6c72ff7c5e7791edab376e10579cbe
Cuong <tr6r20@gmail.com>
2023-11-13 21:20:15 +0700

    update timepicker

--- FlyBugClub_WebApp/FlyBugClub_WebApp/wwwroot/js/datepicker.js
+++ FlyBugClub_WebApp/FlyBugClub_WebApp/wwwroot/js/datepicker.js
@@ -2,6 +2,10 @@
     $('input[name="daterange"]').daterangepicker({
         opens: 'left'
     }, function (start, end, label) {
+        const inputDateRange = document.getElementById('cuong');
+        var result = start.format('YYYY-MM-DD') + "@@" + end.format('YYYY-MM-DD');
+        inputDateRange.classList.add(result);
         console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+
     });
 });
